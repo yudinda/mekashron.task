dbe4d4cafee7c1e47ef0c81e45f35a9c93fb223f
yudinda <yudind@inbox.ru>
2022-04-05 13:19:20 +0300

    Add project files.

---
+++ mekashron.backend/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using icutech;
+using mekashron.backend.Models;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace mekashron.backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountController : Controller
+    {
+        [HttpPost]
+        [Route("Login")]
+        [EnableCors("PolicyLocal")]
+        public async Task<LoginReturnModel> Login(LoginModel model)
+        {
+            ICUTechClient client = new ICUTechClient(ICUTechClient.EndpointConfiguration.IICUTechPort);
+            var response = await client.LoginAsync(model.Username, model.Password, "");
+            //Let's hide details from frontend and return only required data
+            dynamic json = JsonConvert.DeserializeObject(response.@return);
+            var resultModel = new LoginReturnModel();
+            if (json["ResultCode"] != null)
+            {
+                resultModel.Success = false;
+                resultModel.Error = json["ResultMessage"];
+            }
+            else
+            {
+                resultModel.Success = true;
+                resultModel.LastName = json["LastName"];
+                resultModel.FirstName = json["FirstName"];
+            }
+            return resultModel;
+            
+        }
+    }
+}
---
+++ mekashron.backend/Models/LoginReturnModel.cs
@@ -0,0 +1,10 @@
+﻿namespace mekashron.backend.Models
+{
+    public class LoginReturnModel
+    {
+        public bool Success { get; set; }
+        public string Error { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
